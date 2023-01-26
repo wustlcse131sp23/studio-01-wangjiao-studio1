0d7b1f9c6c6556b8b0449b96bd89860e846943de
shederrii <shederrii@localhost>
2023-01-26 13:20:46 -0600

    name

--- src/studio1/HiOne.java
+++ src/studio1/HiOne.java
@@ -10,8 +10,8 @@ public class HiOne {
 	public static void main(String[] args) {
 
 		Scanner in = new Scanner(System.in);
-		System.out.print("Enter value for arg 0: ");
 		String name = in.nextLine();
+		System.out.print("Hello, " + name + ". How are you?");
 		//
 		// Below this line, enter code so that this program's output says
 		//      Hi, Pat.  How are you?
